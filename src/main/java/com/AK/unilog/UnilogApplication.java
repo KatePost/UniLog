6fdaaf9c0bb9272b859d9e76bbfc06403c71a0ce
KatePost <kpost19@gmail.com>
2021-12-09 09:26:50 -0500

    Initial commit

---
+++ src/main/java/com/AK/unilog/UnilogApplication.java
@@ -0,0 +1,13 @@
+package com.AK.unilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnilogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnilogApplication.class, args);
+	}
+
+}
