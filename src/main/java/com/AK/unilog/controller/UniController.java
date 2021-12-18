8575afa2b8fcb137cd12a854edd2d0c9324a3f71
KatePost <kpost19@gmail.com>
2021-12-18 00:15:59 -0500

    trying to debug browser

--- src/main/java/com/AK/unilog/controller/UniController.java
+++ src/main/java/com/AK/unilog/controller/UniController.java
@@ -120,7 +120,7 @@ public class UniController {
             model.addAttribute("form", recoverPasswordForm );
             return "recoverPasswordForm";
         }
-        redirectAttributes.addFlashAttribute("errorMsg", "The link you're trying to access is either expired or incorrect.");
+        redirectAttributes.addFlashAttribute("errorMsg", "The link you're trying to access is either expired or incorrect." + resetToken + "  "  + user.getEmail());
         return "redirect:/login";
     }
 
